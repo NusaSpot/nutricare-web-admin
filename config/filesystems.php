15b6ee1318d2fe098bd2ef3d7f993ea0ef4aadd2
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2023-12-18 02:41:20 +0700

    changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,28 +56,41 @@ return [
             'throw' => false,
         ],
 
+        // 'gcs' => [
+        //     'driver' => 'gcs',
+        //     // 'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', base_path('cloud-storage.json')), // optional: /path/to/service-account.json
+        //     'key_file' => [
+        //         'type' => env('GOOGLE_CLOUD_ACCOUNT_TYPE'),
+        //         'private_key_id' => env('GOOGLE_CLOUD_PRIVATE_KEY_ID'),
+        //         'private_key' => env('GOOGLE_CLOUD_PRIVATE_KEY'),
+        //         'client_email' => env('GOOGLE_CLOUD_CLIENT_EMAIL'),
+        //         'client_id' => env('GOOGLE_CLOUD_CLIENT_ID'),
+        //         'auth_uri' => env('GOOGLE_CLOUD_AUTH_URI'),
+        //         'token_uri' => env('GOOGLE_CLOUD_TOKEN_URI'),
+        //         'auth_provider_x509_cert_url' => env('GOOGLE_CLOUD_AUTH_PROVIDER_CERT_URL'),
+        //         'client_x509_cert_url' => env('GOOGLE_CLOUD_CLIENT_CERT_URL'),
+        //     ],
+        //     'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'), // optional: is included in key file
+        //     'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
+        //     'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', ''), // optional: /default/path/to/apply/in/bucket
+        //     'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_URL', null), // see: Public URLs below
+        //     'apiEndpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
+        //     'visibility' => 'public', // optional: public|private
+        //     'visibility_handler' => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
+        //     'metadata' => ['cacheControl'=> 'public,max-age=86400'], // optional: default metadata
+        // ],
+
         'gcs' => [
             'driver' => 'gcs',
-            // 'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', base_path('cloud-storage.json')), // optional: /path/to/service-account.json
-            'key_file' => [
-                'type' => env('GOOGLE_CLOUD_ACCOUNT_TYPE'),
-                'private_key_id' => env('GOOGLE_CLOUD_PRIVATE_KEY_ID'),
-                'private_key' => env('GOOGLE_CLOUD_PRIVATE_KEY'),
-                'client_email' => env('GOOGLE_CLOUD_CLIENT_EMAIL'),
-                'client_id' => env('GOOGLE_CLOUD_CLIENT_ID'),
-                'auth_uri' => env('GOOGLE_CLOUD_AUTH_URI'),
-                'token_uri' => env('GOOGLE_CLOUD_TOKEN_URI'),
-                'auth_provider_x509_cert_url' => env('GOOGLE_CLOUD_AUTH_PROVIDER_CERT_URL'),
-                'client_x509_cert_url' => env('GOOGLE_CLOUD_CLIENT_CERT_URL'),
-            ],
-            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'), // optional: is included in key file
+            'key_file' => [], // Use default service account
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
-            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', ''), // optional: /default/path/to/apply/in/bucket
-            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_URL', null), // see: Public URLs below
-            'apiEndpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
-            'visibility' => 'public', // optional: public|private
-            'visibility_handler' => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
-            'metadata' => ['cacheControl'=> 'public,max-age=86400'], // optional: default metadata
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', ''),
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_URL', null),
+            'apiEndpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null),
+            'visibility' => 'public',
+            'visibility_handler' => null,
+            'metadata' => ['cacheControl' => 'public,max-age=86400'],
         ],
 
     ],
